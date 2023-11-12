eae7f17dbb4dbee43280c2b8527b898eaf865901
Sharuka Rajapaksha <rajapakshasharuka@gmail.com>
2023-11-12 20:42:22 +0200

    First commit

---
+++ src/Component/Topics.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './topics.css'
+
+export default function Topics() {
+  return (
+    <div>
+         <div className='topics'>
+            <span className='topicText'>This is the Topic</span>
+         </div>
+     
+    </div>
+  )
+}
