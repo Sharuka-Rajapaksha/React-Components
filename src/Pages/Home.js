91ecfe94989b92c15231b46fc386ab3f1d6d8c73
Sharuka Rajapaksha <rajapakshasharuka@gmail.com>
2023-11-15 17:46:47 +0200

    Update Home2.js

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -7,24 +7,22 @@ export default function Home() {
   return (
     <div>
 
-    
-    <Topics/>
-    <Content/>
-   
+      <Topics/>
+      <Content/>
 
-    <div>
-      <Props language = "React"  skill="Excellent">
-          <span>This is Skill number 1: This is Children Prop</span>
-      </Props>
+        <div>
+          <Props language = "React"  skill="Excellent">
+              <span>This is Skill number 1: This is Children Prop</span>
+          </Props>
 
-      <Props language = "JavaScript" skill="Advanced">
-          <button>This is a button</button>
-      </Props>
+          <Props language = "JavaScript" skill="Advanced">
+              <button>This is a button</button>
+          </Props>
 
-      <Props language = "CSS" skill="Intermediate">
-        <p>This is Third Children prop</p>
-      </Props>
-    </div>
+          <Props language = "CSS" skill="Intermediate">
+            <p>This is Third Children prop</p>
+          </Props>
+        </div>
 
     </div>
   )
