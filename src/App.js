87ad7ceca40459f215fdeea7586bf73f38c23c5f
Sharuka Rajapaksha <rajapakshasharuka@gmail.com>
2023-11-16 20:29:34 +0200

    UPDATE App.js

--- src/App.js
+++ src/App.js
@@ -4,12 +4,26 @@ import Home2 from './Pages/Home2';
 import {BrowserRouter as Router,Route,Switch} from 'react-router-dom';
 import About from './Pages/Routing/About';
 import ContactUs from './Pages/Routing/ContactUs';
-
+import { useState } from 'react';
 
 function App() {
+
+  //The value we set for a variable through a useState is not a constant. Can keep the value constant by saving it into a database or a local storage.
+const [count, setCount] = useState(0);
+
+const increment = () =>{
+ setCount(count+10);
+}
+
+const decrement = () => {
+ setCount(count-10);
+}
+
   return (
 
-    <Router>
+  /* React Routing:
+
+  <Router>
       <div>
         <Switch>
           
@@ -28,7 +42,16 @@ function App() {
         </Switch>
       </div>
     </Router>
+  */
   
+  //useState Method:
+<>
+    <span className='title'> My counter</span>
+    <p className='SubTitle'>The count is {count}</p>
+    <button onClick={decrement} className='button'>-</button>
+    <button onClick={increment} className='button' >+</button>
+</>
+
   );
 }
 
